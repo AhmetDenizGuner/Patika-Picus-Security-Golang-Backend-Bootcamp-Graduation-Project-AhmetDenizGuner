7923e6bacdb30d7ddca1f2d3861c50fba72b0917
Ahmet Deniz GÜNER <ahmetdenizguner@gmail.com>
2022-03-29 21:39:32 +0300

    innitial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Patika-Picus-Security-Golang-Backend-Bootcamp-Graduation-Project-AhmetDenizGuner
+
+## ER Diagram
+
+//TODO
+
+
+<img src="/documents/er-diagram.png"/>
---
+++ internal/database/connection.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "host=localhost user=root password=root dbname=library port=3306" //sslmode=disable TimeZone=Asia/Shanghai
+
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		PrepareStmt: true,
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "table_",
+			SingularTable: true,
+		},
+		/*NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},*/
+	})
+
+	if err != nil {
+		panic(fmt.Sprintf("Could not connect to the database: %s", err.Error()))
+	}
+
+	DB = connection
+}
